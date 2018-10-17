ffd87041df76fb95c591080be0774d270e1cd053
Marat91 <santinel_1991@mail.ru>
2018-10-18 00:09:50 +0300

    golden

---
+++ app/js/common.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	$('#my-menu').mmenu({
+		extensions: [ 'theme-black', 'effect-menu-slide', 'pagedim-black' ],
+		navbar: {
+			title: '<img src="img/logo.png" alt="golden">'
+			},
+		offCanvas: {
+			position: "right"
+		}
+	});
+
+	var api = $('#my-menu').data('mmenu');
+
+	api.bind('open:finish', function() {
+		$('.hamburger').addClass('is-active');
+	});
+	api.bind('close:finish', function() {
+		$('.hamburger').removeClass('is-active');
+	});
+	
+$('#hiddenburg').fadeIn();
+
+
+});
